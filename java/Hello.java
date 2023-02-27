514c30e4ffddbf4c289e3dcc8985534bd53d4b31
RoryEiffe <roryeiffe8@gmail.com>
2023-02-27 15:23:52 -0500

    Reorganized files

--- java/Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-    public static void main(String [] args) {
-        // no hoisting:
-        x = 3;
-        int x = 5;
-        System.out.println("Hello World");
-    }
-}
