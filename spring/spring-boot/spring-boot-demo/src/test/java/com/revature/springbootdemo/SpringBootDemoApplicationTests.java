a1f06c8afcc3b0ab80205881518c2bf761430a3f
RoryEiffe <roryeiffe8@gmail.com>
2023-03-21 17:00:37 -0400

    Added Test property source to Spring boot demo

--- spring/spring-boot/spring-boot-demo/src/test/java/com/revature/springbootdemo/SpringBootDemoApplicationTests.java
+++ spring/spring-boot/spring-boot-demo/src/test/java/com/revature/springbootdemo/SpringBootDemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.revature.springbootdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:test.properties")
 class SpringBootDemoApplicationTests {
 
 	@Test
